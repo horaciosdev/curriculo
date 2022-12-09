6431b990bfdd23eab4ecce2c3ae8c9d4db529aad
Horacio <horacio.schumann@gmail.com>
2022-12-09 01:47:36 -0300

    correção do link para ver projetos

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -2,7 +2,7 @@ const ProjectsContainer = () => {
   return (
     <section className="projects-container">
       <h2>Projetos</h2>
-      <a href="https://github.com/hor4cles" target="_blank" className="btn">
+      <a href="https://github.com/horaciosdev" target="_blank" className="btn">
         Ver Projetos
       </a>
     </section>
